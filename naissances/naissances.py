eea420829ca1302eed44ec1dcac92d66f6a304e0
thjbbvlt <thibault2@ik.me>
2023-06-08 23:58:37 +0200

    modifié :         naissances/naissances.py

--- naissances/naissances.py
+++ naissances/naissances.py
@@ -2,8 +2,10 @@
 Observation de la distribution des naissances de la population.
 """
 
-import pandas
 import sparql_dataframe
+
+# import pandas
+# import plotly.express as plt
 
 # L'adresse de DBPedia, où la requête sera adressée
 dbpedia = "http://dbpedia.org/sparql"
@@ -88,11 +90,38 @@ df["birthDate"].replace(
 df.birthDate.sort_values()
 
 # L'opération que j'utilise pour homogénéiser les valeur consiste à enlever les rows dans lequelles la valeur de birthDate est plus grande que 4 caractères.
-new_df = df.drop(df[df['birthDate'].str.len() > 4].index)
+df = df.drop(df[df["birthDate"].str.len() > 4].index)
 
 # Regarder s'il y a des valeurs qui, au contraire, ont moins que 4 caractères.
-df[df['birthDate'].str.len() < 4]
+df[df["birthDate"].str.len() < 4]
 
 # Comme il n'y en a pas, regarder s'il y en a qui commencent par "0".
-df[df['birthDate'].str.startswith('0')]
+df[df["birthDate"].str.startswith("0")]
 
+# Les années qui ont le plus de naissances, et celles qui en ont le moins.
+a = df.birthDate.value_counts()
+a
+
+# Une visualisation du nombres de naissances par années.
+df["birthDate"].value_counts().sort_index().plot(kind="bar")
+
+# Mais cette représentation est inappropriée, car les années sans naissances ne sont pas représentées -- la représentation du temps est donc déformée --, alors que nous voudrions les voir représentées avec la valeur 0.
+years = [(int(i), j) for i, j in df.birthDate.value_counts().items()]
+years[:10]
+
+years_a = [i[0] for i in years]
+years_a.sort()
+
+
+year_start = years_a[0]
+year_end = years_a[-1]
+
+# Construire, pour les besoin de la visualisation, la liste de toutes les années, y compris celles sans naissances, en faisant une liste
+years_all = [i for i in range(int(year_start), int(year_end) + 1, 1)]
+
+y = []
+for i in years_all:
+    if i in dict(years):
+        y.append((i, dict(years)[i]))
+    else:
+        y.append((i, 0))
