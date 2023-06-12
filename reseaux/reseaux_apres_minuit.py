6d58733530fce580dc75ec348544e0d3027407e1
thjbbvlt <thibault2@ik.me>
2023-06-12 02:15:22 +0200

    nouveau fichier : reseaux_apres_minuit.ipynb modifié :         reseaux_apres_minuit.py

--- reseaux/reseaux_apres_minuit.py
+++ reseaux/reseaux_apres_minuit.py
@@ -6,6 +6,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from collections import Counter
 import networkx as nx
 import matplotlib.pyplot as plt
+import re
 
 # L'adresse de DBPedia, où la requête doit être adressée.
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -82,18 +83,20 @@ for i in groups:
 clean_pairs = [
     (
         i.replace("http://dbpedia.org/resource/", ""),
-        j.replace("http://dbpedia.org/resource/", ""),
+        # Je passe les occupations en lowercase pour faciliter
+        # les recherches de caractères.
+        j.replace("http://dbpedia.org/resource/", "").lower(),
     )
     for i, j in pairs
 ]
 clean_pairs[:3]
 
 # Certaines paires me sont inutiles, celles qui ont comme occupation "[nom de la personne]_PersonFunction".
-[i for i in clean_pairs if "PersonFunction" in i[1]][:5]
+[i for i in clean_pairs if "personfunction" in i[1]][:5]
 
 # Je construis donc une nouvelle liste sans ces paires.
 non_trivial_pairs = [
-    i for i in clean_pairs if "PersonFunction" not in i[1]
+    i for i in clean_pairs if "personfunction" not in i[1]
 ]
 len(non_trivial_pairs)
 
@@ -105,12 +108,11 @@ len(uniqpairs)
 names = [i for i, j in uniqpairs]
 names[:3]
 
-names_repeated = [i for i in names if names.count(i) > 1]
-
 # Le nombre total d'occurences de noms.
 len(names)
 
 # Le nombre d'occurences de noms qui apparaissent plusieurs fois.
+names_repeated = [i for i in names if names.count(i) > 1]
 len(names_repeated)
 
 # Le nombre de noms qui apparaissent plusieurs fois. (Les données sont plus minces que ce que je pouvais espérer et il faudrait probablement opter pour une autre manière d'interroger DBPedia.)
@@ -155,45 +157,71 @@ for i in data_to_pop:
 # Le nombre d'entrées restantes dans le dictionnaires.
 len(d)
 
-# Il va désormais s'agir d'explorer les relations entre occupations en utilisant l'analyse de réseaux. Pour créer un réseau reliant les occupations les unes aux autres, il faut d'abord créer des relations entre occupations, des nouvelles paires occupation/occupation.
+# Il va désormais s'agir d'explorer les relations entre occupations en utilisant l'analyse de réseaux. Pour le début de l'exploration, je vais simplifier les données et regarder les relations d'occupations uniquement, en excluant les personnes des graphes, ou plus exactement: je vais créer un graphe avec un seul type de noeud, les occupations, et en réduisant les personnes à des nombres qui renforceront le poids des relations entre les noeuds-occupations. Plus une relation entre deux occupations (deux noeuds) sera répandue, et plus le lien entre elles sera considéré comme lourd.
+# Pour créer un réseau reliant les occupations les unes aux autres, il faut d'abord créer des relations entre occupations, des nouvelles paires occupation/occupation.
 occ_pairs = []
 for name, occupations in d.items():
     for occ_one in occupations:
-        a = occupations.copy()
-        a.remove(occ_one)
-        for occ_two in a:
-            p = (occ_one, occ_two)
-            occ_pairs.append(p)
+        for occ_two in occupations:
+            if occ_two != occ_one:
+                o = [occ_one, occ_two]
+                o.sort()
+                p = (o[0], o[1])
+                occ_pairs.append(p)
 
 # Certaines paires ont plusieurs occurences: quand deux occupations cohabitent chez plusieurs personnes.
-[i for i in occ_pairs if 'Biographer' in i][:10]
+[i for i in occ_pairs if "biographer" in i and "novelist" in i][:10]
 
-# Plutôt que d'avoir ainsi des doublons, on peut représenter le nombre d'occurences comme la force de la relation.
-c = Counter(occ_pairs)
-relations = [(i[0], i[1], j) for i, j in list(c.items())]
-relations[:10]
+# Plutôt que d'avoir ainsi des doublons, on peut représenter le nombre d'occurences comme la force de la relation. Observer les relations avec le plus d'occurences.
+count = Counter(occ_pairs)
+count.most_common()[:10]
 
-# Créer un graphe avec ces relations.
-G = nx.Graph()
-for relation in relations:
-    node1, node2, weight = relation
-    G.add_edge(node1, node2, weight=weight)
+# Pour produire une première visualisation, je fais une sous-liste des paires d'occupations les plus courantes.
+relations_most_common = [
+    (i[0], i[1], j)
+    for i, j in list(count.most_common()[:30])
+    if "writer" not in i
+]
+relations_most_common[:10]
 
-# G.
+# Construction d'un graphe avec ces paires d'occupations.
+graph_most_common = nx.Graph()
+for relation in relations_most_common:
+    node1, node2 = relation[0], relation[1]
+    graph_most_common.add_edge(node1, node2)
+
 
 # Tracer le graphe
-pos = nx.spring_layout(G)  # Positions des nœuds
+def tracer_montrer_graphe_sans_poids(graph):
 
-# Récupérer les poids des arêtes
-edge_weights = [relation[2] for relation in rel[:10]]
+    # Positions des nœuds
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_edges(graph, pos, edge_color="gray")
 
-# Tracer les arêtes avec une épaisseur basée sur les poids
-nx.draw_networkx_edges(G, pos, width=edge_weights, edge_color='gray')
+    # Tracer les nœuds et les étiquettes
+    nx.draw_networkx_nodes(
+        graph, pos, node_color="lightblue", node_size=800
+    )
+    nx.draw_networkx_labels(graph, pos, font_size=12)
 
-# Tracer les nœuds et les étiquettes
-nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=800)
-nx.draw_networkx_labels(G, pos, font_size=12)
+    # Afficher le graphe
+    plt.axis("off")
+    plt.show()
 
-# Afficher le graphe
-plt.axis('off')
-plt.show()
+tracer_montrer_graphe_sans_poids(graph_most_common)
+
+# Cette première visualisation nous montre déjà des choses intéressantes, quoi que relativement triviales. Le graphe est séparé en deux clusters: les écritures littéraires et le journalisme d'un côté, les activités liées au cinéma ou à la télévision de l'autre. Ce premier découpage est intéressant du fait de l'annexion de l'occupation journalistique aux écritures littéraires. Le journalisme, du point de vue de ce graphe, peut bien apparaître, d'une part, comme un métier d'appoint typique des littérateurices; et d'autre part comme un espace d'expression publique dont les littérateurices se saisissent davantage que d'autres groupes d'écrivain-es. Surtout, ce découpage en deux clusters est moins un découpage entre deux types d'activités qu'entre deux "mondes de l'art" au sens de Howard Becker. Car, d'un point de vue technique, technologique, physiologique, etc., l'écriture pour la télévision est bien plus proche de l'écriture d'une pièce de théâtre que de l'acting. À moins qu'on entende par "écriture" un ensemble large de pratiques intégrant les formes de collaboration (réécrire sous la contrainte des équipes de tournages, discuter, reprendre des parties de scripts existants, etc.), auquel cas "l'écriture" du screenwriter a en effet beaucoup à voir avec l'acting, puisqu'il s'agit essentiellement d'une activité consistant à employer les mêmes critères et à cotoyer les mêmes personnes: celles et ceux du monde du cinéma.
+# Par ailleurs, on peut voir, dans le cluster lié au cinéma, que les nodes 'actor' et 'television writer' sont très éloigné, tandis que les nodes 'actor' et 'screen writer' (l'écriture pour le cinéma) sont plus rapprochés: on peut ainsi faire l'hypothèse que le monde de la télévision n'est pas structuré avec la même souplesse que le monde du cinéma, et que l'assignation à des rôles professionnels définis y est plus forte. Toutefois, il faut fortement relativiser cette hypothèse: ces résultats sont probablement liés au biais induit par la sélection des paires d'occupations les plus courantes.
+
+
+# Fonction pour chercher les nodes connectés aux nodes contenant un mot ou une chaîne de caractère.
+def connected_nodes(mot: str, graph=graph_most_common):
+    a = [node for node in graph.nodes() if mot in node]
+    b = []
+    for i in a:
+        b.extend(graph_most_common.neighbors(i))
+    return (a, b)
+
+
+# Les nodes connectés à des nodes contenant le mot 'book'
+connected_nodes("book")
